7348a26373bebeefa173bb0c592aa796edf0fbef
0rr <sethwbrown22@gmail.com>
2018-04-24 03:17:20 +0000

    Added place edit form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -21,6 +21,13 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
   end
 
+  def edit
+    @place = Place.find(params[:id])
+  end
+
+
+
+
   private
 
   def place_params
